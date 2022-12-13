9b9c5e381cdab9827b05092ef99bf729aad826d6
Allyson McInnis <allysonmcinnis@Allysons-Air.lan>
2022-12-12 18:52:02 -0600

    add .py and edit

---
+++ Resources/data.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+# to read csv file named "samplee"
+a = pd.read_csv("Resources/cities.csv")
+
+# to save as html file
+# named as "Table"
+a.to_html("Resources/Table.html")
+
+# assign it to a
+# variable (string)
+html_file = a.to_html()
